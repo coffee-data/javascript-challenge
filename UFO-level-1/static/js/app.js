cc61a454194cad3fcf0b8b24c7400dd5ff8d5e18
coffee-data <jesse.ryan.hartman@gmail.com>
2020-05-11 13:35:13 -0500

    Submit working
    Submit is returning a completed data set for the query but it only appends.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -49,21 +49,13 @@ function runEnter() {
 
     console.log(filteredData);
 
-}
-
-// button.on("click", function(){
-//     // Prevent the page from refresh
-//     d3.event.preventDefault();
-
-//     // Select the input element and get the raw HTML node
-//     var inputElement = d3.select("#datetime");
-
-//     // Get the value property of the input element
-//     var inputValue = inputElement.property("value");
-
-//     console.log(inputValue);
-//     console.log(tableData);
-
-//     // Use the form input to filter the data by date
-//     var filterData = tableData.filter()
-// })
+    filteredData.forEach(function(dataReport) {
+        console.log(dataReport);
+        // Create empty rows for data query results
+        var row = tbody.append("tr");
+        Object.entries(dataReport).forEach(function([key,value]) {
+            var cell = row.append("td");
+            cell.text(value);
+        })
+    });
+};
