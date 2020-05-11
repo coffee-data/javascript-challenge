366d58360c08e361ce88ba76d360b831435edc1e
coffee-data <jesse.ryan.hartman@gmail.com>
2020-05-11 13:22:22 -0500

    Update to app.js
    The data.datetime filter is functional.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,32 +1,20 @@
 // from data.js
 var tableData = data;
 
-// d3 selections
-var tbody = d3.select("tbody")
+// Select the button
 var button = d3.select("#filter-btn");
-var inputDate = d3.select("#datetime");
 
-// // Event handler for button click
-// button.on("click", someFunction());
-// formDate.on("submit", someFunction());
+// Select the form
+var form = d3.select("form");
 
-button.on("click", function(){
-    // Prevent the page from refresh
-    d3.event.preventDefault();
+// Select the table body element
+var tbody = d3.select("tbody");
 
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
+// Event handlers
+button.on("click", runEnter);
+form.on("submit", runEnter);
 
-    // Get the value property of the input element
-    var inputValue = inputElement.property("value");
-
-    console.log(inputValue);
-    console.log(tableData);
-
-    // Use the form input to filter the data by date
-    var filterData = tableData.filter()
-})
-
+// // Create the table and display
 // data.forEach(function(dataReport) {
 //     console.log(dataReport);
 //     var row = tbody.append("tr");
@@ -38,3 +26,44 @@ button.on("click", function(){
 //     cell.text(value);
 //     });
 // });
+
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+    console.log(inputElement);
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(tableData);
+
+    // Use the form input to filter the data by date
+    var filteredData = tableData.filter(function(data) {
+        return data.datetime === inputValue;
+    })
+
+    console.log(filteredData);
+
+}
+
+// button.on("click", function(){
+//     // Prevent the page from refresh
+//     d3.event.preventDefault();
+
+//     // Select the input element and get the raw HTML node
+//     var inputElement = d3.select("#datetime");
+
+//     // Get the value property of the input element
+//     var inputValue = inputElement.property("value");
+
+//     console.log(inputValue);
+//     console.log(tableData);
+
+//     // Use the form input to filter the data by date
+//     var filterData = tableData.filter()
+// })
